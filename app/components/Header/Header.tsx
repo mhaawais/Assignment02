cb82193738cf4366b0298b9295ca9935f80aee90
M Awais <awais23112001@gmail.com>
2024-10-16 11:13:23 +0500

    first commit

---
+++ app/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from "./Header.module.css"
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <h1></h1>This is an HEADER component
+      <p></p>This is my Homepage
+    </header>
+  )
+}
+
+export default Header
---
+++ app/components/Hero/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from "./Hero.module.css"
+
+const Hero = () => {
+  return (
+    <section className={styles.hero}>
+            <h2></h2>This is an HERO component
+            <p></p>This is my Components App
+            <h3></h3>Hello Guys!
+            <h4></h4>CTA (section)
+    </section>
+  )
+}
+
+export default Hero
